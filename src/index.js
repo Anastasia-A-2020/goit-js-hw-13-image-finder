682234c3ce47d0ca57262f1b695863dac32b5616
Anastasia-Aleksandrova <a-a2018@ukr.net>
2021-05-31 23:33:21 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
+import imgCardTpl from './templates/imgCardTpl.hbs';
 import './sass/main.scss';
+import { searchForm, gallery, loadMoreButton } from './js/refs';
+import ImagesApiService from './js/apiService';
+import LoadMoreBtn from './js/loadMoreBtn';
+
+searchForm.addEventListener('submit', onSearch);
+
+const imgApiService = new ImagesApiService();
+
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+
+loadMoreBtn.refs.button.addEventListener('click', getImages);
+
+function onSearch(event) {
+  event.preventDefault();
+  imgApiService.query = event.currentTarget.elements.query.value;
+
+  loadMoreBtn.show();
+  imgApiService.resetPage();
+  clearGallery();
+  getImages();
+}
+
+function getImages() {
+  loadMoreBtn.disable();
+  imgApiService.fetchImages().then(images => {
+    imgMarkup(images);
+    loadMoreBtn.enable();
+  });
+}
+
+function imgMarkup(images) {
+  const imgMarkup = imgCardTpl(images);
+  //console.log(imgMarkup);
+  gallery.insertAdjacentHTML('beforeend', imgMarkup);
+  handleButtonClick();
+}
+
+function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+function handleButtonClick() {
+  gallery.scrollIntoView({ block: 'end', behavior: 'smooth' });
+}
---
+++ src/js/refs.js
@@ -0,0 +1,3 @@
+export const searchForm = document.getElementById('search-form');
+export const gallery = document.querySelector('.gallery');
+export const loadMoreButton = document.querySelector('.btn-load');
